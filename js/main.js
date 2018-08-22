af499d3ca36df0b8c2a2d20b1a27c82ae99c3395
balenakoi <39910332+balenakoi@users.noreply.github.com>
2018-08-22 20:12:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,173 +1,100 @@
-var game = {
-  tieRound: " This round is a draw.",
-  winRound: " You win this round.",
-  loseRound: " You lose this round.",
-  winGameHead: "Congratulations!",
-  loseGameHead: "You lost this game.",
-  winGameMsg: "You&rsquo;ve won the game!",
-  loseGameMsg: "Better luck next time!",
-  rockScissors: "Rock breaks scissors.",
-  scissorsPaper: "Scissors cut paper.",
-  paperRock: "Paper covers rock."
-};
+var userScore = 0;
+var computerScore = 0;
+var userScore_span = document.getElementById("user-score");
+var computerScore_span = document.getElementById("computer-score");
+var scoreBoard_div = document.querySelector(".score-board");
+var result_p = document.querySelector(".result > p");
+var rock_div = document.getElementById("rock");
+var paper_div = document.getElementById("paper");
+var scissor_div = document.getElementById("scissor");
 
-var player = {
-  move: "?",
-  score: 0
-};
-
-var computer = {
-  moves: ["rock", "paper", "scissors"],
-  move: "?",
-  score: 0
-};
-
-// DOM HANDLES
-var playerMove = document.getElementById("playerMove");
-var computerMove = document.getElementById("computerMove");
-var playByPlay = document.getElementById("playByPlay");
-var turnScore = document.getElementById("turnScore");
-var playerPt = document.getElementsByClassName("playerPt");
-var computerPt = document.getElementsByClassName("computerPt");
-var gameOverBox = document.getElementById("gameOverBox");
-var gameOverHead = document.getElementById("gameOverHead");
-var gameOverMsg = document.getElementById("gameOverMsg");
-
-
-document.getElementById("validatname").onclick = function() {name()};
-function name(){
-  var name = document.getElementById("entername").value;
-  if(name.length >= 2) {
-     document.getElementById("name").style.display="none";
-     document.getElementById("user-label").innerHTML = name;
-     document.getElementById("game").style.display="inline-block";
-   }
- }
-
-// GAME FUNCTIONS
-function getPlayerMove(move) {
-  player.move = move;
-  getcomputerMove();
-  displayMoves();
-  resolveTurn();
-  checkForWin();
-}
-
-function getcomputerMove() {
-  computer.move = computer.moves[ Math.floor(Math.random() * 3) ];
-}
-
-function displayMoves() {
-  switch (player.move) {
-    case "rock":
-      playerMove.innerHTML = "<i class='fa fa-hand-rock-o'></i>";
-      break;
-    case "paper":
-      playerMove.innerHTML = "<i class='fa fa-hand-paper-o'></i>";
-      break;
-    default: // "scissors"
-      playerMove.innerHTML = "<i class='fa fa-hand-scissors-o fa-flip-horizontal'></i>";
-      break;
-    }
-
-  switch (computer.move) {
-    case "rock":
-      computerMove.innerHTML = "<i class='fa fa-hand-rock-o'></i>";
-      break;
-    case "paper":
-      computerMove.innerHTML = "<i class='fa fa-hand-paper-o'></i>";
-      break;
-    default: // "scissors"
-      computerMove.innerHTML = "<i class='fa fa-hand-scissors-o'></i>";
-      break;
-    }
-}
-
-function resolveTurn() {
-  if ( player.move === computer.move ) {
-    playByPlay.innerHTML = ( "Both players chose " + player.move + ".");
-    turnScore.innerHTML = ( game.tieRound );
-  }
-
-  else {
-    switch (player.move) {
-      case "rock":
-        if ( computer.move === "paper" ) {
-          playByPlay.innerHTML = ( game.paperRock );
-          computerScore();
-        }
-        else {
-          playByPlay.innerHTML = ( game.rockScissors );
-          incPlayerScore();
-        }
-        break;
-
-      case "paper":
-        if ( computer.move === "scissors" ) {
-          playByPlay.innerHTML = ( game.scissorsPaper );
-          computerScore();
-        }
-        else {
-          playByPlay.innerHTML = ( game.paperRock);
-          incPlayerScore();
-        }
-        break;
-
-      default: // "scissors"
-        if ( computer.move === "rock" ) {
-          playByPlay.innerHTML = ( game.rockScissors );
-          computerScore();
-        }
-        else {
-          playByPlay.innerHTML = ( game.scissorsPaper );
-          incPlayerScore();
-        }
+ document.getElementById("validatname").onclick = function() {name()};
+ function name(){
+   var name = document.getElementById("entername").value;
+   if(name.length >= 2) {
+      document.getElementById("name").style.display="none";
+      document.getElementById("user-label").innerHTML = name;
+      document.getElementById("allgame").style.display="inline-block";
     }
   }
+
+
+function getComputerChoice(){
+  var choices = ["rock", "paper","scissor"];
+  var randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+
 }
 
-function incPlayerScore() {
-  turnScore.innerHTML = ( game.winRound );
-  playerPt[player.score].setAttribute( "style", "background-color: royalblue;" );
-  player.score++;
+
+function win(userChoice, computerChoice){
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  var smallUserWord = "user".fontsize(3).sup();
+  var smallComputerWord = "computer".fontsize(3).sup();
+  result_p.innerHTML = userChoice + smallUserWord + " " +  "beats" + " " +  computerChoice  + smallComputerWord + " " + ".You win!";
+  document.getElementById(userChoice).classList.add("green-glow");
+  document.getElementById("myImg").src = "img/body2.gif";
 }
 
-function computerScore() {
-  turnScore.innerHTML = ( game.loseRound );
-  computerPt[computer.score].setAttribute( "style", "background-color: orange;" );
-  computer.score++;
+function lose(userChoice, computerChoice){
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  var smallUserWord = "user".fontsize(3).sup();
+  var smallComputerWord = "computer".fontsize(3).sup();
+  result_p.innerHTML = userChoice + smallUserWord + " " +  "loses to" + " " +  computerChoice  + smallComputerWord + " " + ".You lost!";
+  document.getElementById("myImg").src = "img/body3.gif";
 }
 
-function checkForWin() {
-  if (player.score === 3) {
-    gameOverBox.setAttribute(
-      "style",
-      "display: block; background-color: lightgreen; border-color: darkgreen;"
-    );
-    gameOverHead.innerHTML = game.winGameHead;
-    gameOverMsg.innerHTML = game.winGameMsg;
+function draw(userChoice, computerChoice){
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  var smallUserWord = "user".fontsize(3).sup();
+  var smallComputerWord = "computer".fontsize(3).sup();
+  result_p.innerHTML = userChoice + smallUserWord + " " +  "equals" + " " +  computerChoice  + smallComputerWord + " " + ".It is a draw!";
+  document.getElementById("myImg").src = "img/body4.gif";
+}
+
+function main() {
+  rock_div.addEventListener("click", function(){
+    play("rock");
+  })
+
+  paper_div.addEventListener("click", function(){
+    play("paper");
+  })
+
+  scissor_div.addEventListener("click", function(){
+    play("scissor");
+  })
+}
+
+main();
+
+function play(userChoice){
+  var computerChoice = getComputerChoice();
+  switch(userChoice + computerChoice) {
+  case "rockscissor":
+  case "paperrock":
+  case "scissorpaper":
+  win(userChoice, computerChoice);
+  break;
+  case "rockpaper":
+  case "paperrock":
+  case "scissorrock":
+  lose(userChoice, computerChoice);
+  break;
+  case "rockrock":
+  case "paperpaper":
+  case "scissorscissor":
+  draw(userChoice, computerChoice);
+  break;
   }
-  else if (computer.score === 3) {
-    gameOverBox.setAttribute(
-      "style",
-      "display: block; background-color: rgb(256,164,164); border-color: rgb(128,0,0);"
-    );
-    gameOverHead.innerHTML = game.loseGameHead;
-    gameOverMsg.innerHTML = game.loseGameMsg;
+  if(userScore== 3){
+    alert("j'ai gagne");
+  }if(computerScore === 3){
+    alert("vous avez perdu");
   }
-}
-
-
-function playAgain() {
-  gameOverBox.setAttribute("style", "display: none;");
-  playByPlay.innerHTML = "Playing again?";
-  turnScore.innerHTML = "Choose your first move.";
-  for (var i = 0; i < 3; i++) {
-    playerPt[i].setAttribute("style", "background-color: #ccc;");
-    computerPt[i].setAttribute("style", "background-color: #ccc;");
-  }
-  playerMove.innerHTML = ("<i class='fa fa-question'></i>");
-  computerMove.innerHTML = ("<i class='fa fa-question'></i>");
-  player.score = 0;
-  computer.score = 0;
 }
